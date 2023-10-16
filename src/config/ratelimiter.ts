35645fd21d5b35381c5c8fcb77b24062a9c0fd2b
unknown <95885719+fotmjay@users.noreply.github.com>
2023-10-16 18:15:41 -0400

    fixed ratelimiter config to work on render

--- src/config/ratelimiter.ts
+++ src/config/ratelimiter.ts
@@ -3,5 +3,6 @@ export const rateLimiterConfig = {
   limit: 2, // Limit each IP to Y requests per window.
   standardHeaders: "draft-7", // draft-6: `RateLimit-*` headers; draft-7: combined `RateLimit` header
   legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+  validate: { xForwardedForHeader: false },
   // store: ... , // Use an external store for more precise rate limiting
 };

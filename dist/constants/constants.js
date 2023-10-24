0db3133f1c39d1acf4008270cca7aad75466b867
unknown <95885719+fotmjay@users.noreply.github.com>
2023-10-24 16:07:42 -0400

    removed unused module, now sends publickey on noncerequest to easily assess encryption enabled or not for client

--- dist/constants/constants.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.CONSTANT = void 0;
-exports.CONSTANT = {
-    CONTRACTADDRESS: "5GEDATiqzWkVu98jdSi2EuJ2Xtee2KoB7fMoRnJPTVAvqoVq",
-    PROVIDER: "wss://ws.azero.dev",
-};
--- dist/constants/endpointForApi.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.APIENDPOINTS = exports.APIBASEURL = void 0;
-exports.APIBASEURL = {
-    aleph: "https://alephzero.api.subscan.io",
-};
-exports.APIENDPOINTS = {
-    contractTransactions: "/api/scan/contracts/transactions",
-};

3032a3d13070699d6433fcbcb146b4e2429c5a5c
unknown <95885719+fotmjay@users.noreply.github.com>
2023-11-02 14:03:28 -0400

    changed rate limit to 5 requests per 10 seconds

--- src/config/ratelimiter.ts
+++ src/config/ratelimiter.ts
@@ -1,6 +1,6 @@
 export const rateLimiterConfig = {
   windowMs: 10 * 1000, // Window time:  X seconds
-  limit: 10, // Limit each IP to Y requests per window.
+  limit: 5, // Limit each IP to Y requests per window.
   standardHeaders: "draft-7", // draft-6: `RateLimit-*` headers; draft-7: combined `RateLimit` header
   legacyHeaders: false, // Disable the `X-RateLimit-*` headers
   validate: { xForwardedForHeader: false },
